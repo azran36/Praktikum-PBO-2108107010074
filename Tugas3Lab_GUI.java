169340325a5087deb9855b1c0f31571997be26fb
azran36 <112817723+azran36@users.noreply.github.com>
2022-11-23 02:01:07 +0700

    Add files via upload

---
+++ Tugas3Lab_GUI.java
@@ -0,0 +1,16 @@
+
+/**
+ *
+ * @author Azran
+ */
+public class Tugas3Lab_GUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
